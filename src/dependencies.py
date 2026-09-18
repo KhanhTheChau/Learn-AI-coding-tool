48085e23c7aebca1968cf4e2733b78e5044dfcab
14224384 <khanhchau5678@gmail.com>
2026-09-18 19:43:17 +0700

    refactor: implement ThirdPartyVideoProvider and Fallback Mock Mode

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,11 +1,11 @@
 from src.repositories.job_repository import InMemoryJobRepository
 from src.ai.video_generator import AIVideoGenerator
-from src.pipeline.video_assembler import VideoAssembler
+from src.pipeline.third_party_provider import ThirdPartyVideoProvider
 
 # Global instances
 _job_repository = InMemoryJobRepository()
 _ai_video_generator = AIVideoGenerator()
-_video_assembler = VideoAssembler()
+_video_provider = ThirdPartyVideoProvider()
 
 def get_job_repository() -> InMemoryJobRepository:
     return _job_repository
@@ -13,5 +13,5 @@ def get_job_repository() -> InMemoryJobRepository:
 def get_ai_video_generator() -> AIVideoGenerator:
     return _ai_video_generator
 
-def get_video_assembler() -> VideoAssembler:
-    return _video_assembler
+def get_video_provider() -> ThirdPartyVideoProvider:
+    return _video_provider
